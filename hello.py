7b0823ce75344ed0f70b661085eabc3f3c55203e
skandanka <skandanka@example.com>
2021-12-15 15:32:34 +0530

    thirsty water

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
 print("Hello world")
+hungry=input("Are you Hungry?")
+if hungry=="yes":
+    print("Eat Something")
+else:
+    thirsty=input("Are you Thirsty?")
+    if thirsty=="yes":
+        print("Drink Water")
+    else:
+        print("cool")
